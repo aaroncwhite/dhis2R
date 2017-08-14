217fa65ce7f918725d33c7d6a80a36f1c9fb33e2
Aaron <aaroncwhite@gmail.com>
2017-08-14 16:28:41 -0400

    fixed cloneDHIS2_data parameters

--- example_scripts/Cloning Data.R
+++ example_scripts/Cloning Data.R
@@ -1,4 +1,6 @@
-# Sample Update Script
+# Sample Cloning Script
+# Assumes you already have all metadata in place from the source server in the destination server
+
 dhis2R_dir <- 'dhis2R_location' # "./" or "/home/aaron/dhis2R" etc
 location <- 'some_location_in_credentials' # Edit the credentials file to make sure the name matches.  Could be something like 'haiti', 'malawi', etc
 
@@ -7,4 +9,4 @@ setwd(dhis2R_dir)
 
 setDHIS2_credentials(location)
 
-cloneDHIS2_data(usr.src, pwd.src, url.src, usr.dest, pwd.dest, url.dest)
+cloneDHIS2_data(usr.src, pwd.src, url.src, usr.dest, pwd.dest, url.dest, match_on = 'displayName', match_on_prefix = '')

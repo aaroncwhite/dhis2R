a27471d9647159c42d6636e3f704541b7b4952a0
Aaron <aaroncwhite@gmail.com>
2017-08-14 16:35:53 -0400

    add sourcing of all modules

--- example_scripts/Cloning Data.R
+++ example_scripts/Cloning Data.R
@@ -1,12 +1,18 @@
 # Sample Cloning Script
 # Assumes you already have all metadata in place from the source server in the destination server
 
-dhis2R_dir <- 'dhis2R_location' # "./" or "/home/aaron/dhis2R" etc
+dhis2R_dir <- '/home/dhis/R tool' # "./" or "/home/aaron/dhis2R" etc
 location <- 'some_location_in_credentials' # Edit the credentials file to make sure the name matches.  Could be something like 'haiti', 'malawi', etc
+
 
 # The script-------------------------------------------------
 setwd(dhis2R_dir)
 
+# load all of the packages
+source('settings.R')
+
+# set credentials
 setDHIS2_credentials(location)
 
+# run the update
 cloneDHIS2_data(usr.src, pwd.src, url.src, usr.dest, pwd.dest, url.dest, match_on = 'displayName', match_on_prefix = '')
